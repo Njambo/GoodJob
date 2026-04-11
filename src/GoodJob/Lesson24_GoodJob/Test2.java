e7b8769ab62fcc3f00f2fee44fbf65937727a564
Njambo <nikita.bryakotnin@mail.ru>
2026-04-11 12:26:48 +0700

    Надо До делать

--- src/GoodJob/Lesson24_GoodJob/Test2.java
+++ src/GoodJob/Lesson24_GoodJob/Test2.java
@@ -3,24 +3,47 @@ package GoodJob.Lesson24_GoodJob;
 public class Test2 {
     public static void main(String[] args) {
 
-}
+    }
 
-abstract class Figure {
-    int numberOfSides = 0;
-    abstract void perimeter();
-    abstract void Area();
-    void showInfo() {
-        System.out.println("Эта фигура ");
+    abstract class Figure {
+        int numberOfSides = 0;
+
+        abstract void perimeter();
+
+        abstract void area();
+
+        void showInfo() {
+            System.out.println("Эта фигура ");
+        }
+    }
+
+
+    class Square extends Figure {
+        int numberOfSides = 4;
+        int side = 10;
+
+        public void perimeter() {
+            System.out.println("Периметр квадрата = " + 4 * side);
+        }
+
+        public void area() {
+            System.out.println("Площадь квадрата = " + side * side);
+        }
     }
 }
 
-
-class Square extends Figure {
-
-}
-
 class rectangle extends Figure {
+    int numberOfSides = 4;
+    int side1 = 8;
+    int side2 = 5;
 
+    public void perimeter() {
+        System.out.println("Периметр прямоугольника = " + 2 * (side1 + side2));
+    }
+
+    public void area() {
+
+    }
 }
 
 class Circles extends Figure {
